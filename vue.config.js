71563aefb73621580b153f36b0f7f11e737cf950
郭斌勇 <guobinyong@qq.com>
2020-07-06 14:07:40 +0800

    增加主题变量更改配置

--- vue.config.js
+++ vue.config.js
@@ -21,4 +21,20 @@ module.exports = {
         }
     },
     // transpileDependencies:["com-tools","by-http","done-count","by-browser"],
+    css: {
+        loaderOptions: {
+          less: {
+            // 若使用 less-loader@5，请移除 lessOptions 这一级，直接配置选项。
+            lessOptions: {
+              modifyVars: {
+                // 直接覆盖变量
+                // 'text-color': '#111',
+                // 'border-color': '#eee',
+                // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+                // hack: `true; @import "your-less-file-path.less";`,
+              },
+            },
+          },
+        },
+      }
 }
